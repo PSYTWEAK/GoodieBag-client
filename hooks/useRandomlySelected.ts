2b599806cb7840946a6e8fc47f4caf7c6d5aedbf
PSYTWEAK <psy@promise.exchange>
2022-07-26 17:49:11 +0100

    improvements to useRandomlySelected.ts added filtering of removeNoneEthPools, removeStables, removeBlueChips and removeLowVolume

--- hooks/useRandomlySelected.ts
+++ hooks/useRandomlySelected.ts
@@ -5,7 +5,76 @@ import useArbitrumSubgraph from "./subgraph/useArbitrumSubgraph";
 export default async function useRandomlySelected() {
   const result = await useArbitrumSubgraph();
 
-  console.log(result.data.poolDayDatas);
+  let pools: any = result.data.poolDayDatas;
 
-  return result;
+  pools = removeBlueChips(pools);
+  pools = removeStables(pools);
+  pools = removeNoneEthPools(pools);
+  pools = removeLowVolume(pools);
+
+  return pools;
+}
+
+const blueChips = [
+  //wbtc
+  "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+];
+const stables = [
+  //usdc
+  "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+  //dai
+  "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+  //usdt
+  "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+  //frax
+  "0x17fc002b466eec40dae837fc4be5c67993ddbd6f",
+  //mim
+  "0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a",
+];
+const weth = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1";
+
+function removeNoneEthPools(pools: any): any {
+  let _pools = pools;
+  const index = _pools.findIndex((data: any) => data.pool.token0.id != weth && data.pool.token1.id != weth);
+  if (index > -1) {
+    _pools.splice(index, 1);
+    return removeNoneEthPools(_pools);
+  } else {
+    return _pools;
+  }
+}
+
+function removeStables(pools: any): any {
+  let _pools = pools;
+  for (let i = 0; i < stables.length; i++) {
+    const index = _pools.findIndex((data: any) => data.pool.token0.id === stables[i] || data.pool.token1.id === stables[i]);
+    if (index > -1) {
+      _pools.splice(index, 1);
+      i--;
+    }
+  }
+  return _pools;
+}
+
+function removeBlueChips(pools: any): any {
+  let _pools = pools;
+  for (let i = 0; i < blueChips.length; i++) {
+    const index = _pools.findIndex((data: any) => data.pool.token0.id === blueChips[i] || data.pool.token1.id === blueChips[i]);
+    if (index > -1) {
+      _pools.splice(index, 1);
+      i--;
+    }
+  }
+
+  return _pools;
+}
+function removeLowVolume(pools: any): any {
+  let _pools = pools;
+  const index = _pools.findIndex((data: any) => data.pool.volumeUSD < 1000);
+  if (index > -1) {
+    _pools.splice(index, 1);
+    return removeLowVolume(_pools);
+  } else {
+    return _pools;
+  }
 }
