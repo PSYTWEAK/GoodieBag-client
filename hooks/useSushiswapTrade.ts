ff453652c6c9b1abb7e50b20692294899b5ab545
PSYTWEAK <psy@promise.exchange>
2022-08-15 21:34:51 +0100

    Added 1inch logic to useSushiTrade.ts

--- hooks/useSushiswapTrade.ts
+++ hooks/useSushiswapTrade.ts
@@ -1,35 +1,51 @@
-import { SushiswapPair, WETH, ChainId, TradeContext } from "simple-sushiswap-sdk";
+import fetch from "node-fetch";
+import JSBI from "jsbi";
+import { weth } from "./stratergies/globals";
+import { arbiTokenEaterAddress } from "../globals";
 
-const sushiswapPair = new SushiswapPair({
-  // use the WETH import from the lib, bare in mind you should use the
-  // network which yours on, so if your on rinkeby you should use
-  // WETH.RINKEBY
-  fromTokenContractAddress: WETH.MAINNET().contractAddress,
-  // the contract address of the token you want to convert TO
-  toTokenContractAddress: "0x111111111117dC0aa78b770fA6A738034120C302",
-  // the ethereum address of the user using this part of the dApp
-  ethereumAddress: "0xB1E6079212888f0bE0cf55874B2EB9d7a5e02cD9",
-  // you can pass in the provider url as well if you want
-  // providerUrl: YOUR_PROVIDER_URL,
-  chainId: ChainId.MAINNET,
-});
+let callData: any = [];
+let tokenId: any = [];
+let value = JSBI.BigInt(0);
 
-// now to create the factory you just do
-const sushiswapPairFactory = await sushiswapPair.createFactory();
+export default async function useSushiswapTrade(provider: any, tokens: any, slippage: number, amountIn: BigNumber) {
+  const chainId = provider._network.chainId;
 
-// the amount is the proper entered amount
-// so if they enter 10 pass in 10 and
-// it will work it all out for you
-const trade = await sushiswapPairFactory.trade("10");
+  const apiBaseUrl = "https://api.1inch.io/v4.0/" + chainId;
 
-// subscribe to quote changes
-trade.quoteChanged$.subscribe((value: TradeContext) => {
-  // value will hold the same info as below but obviously with
-  // the new trade info.
-});
+  const amountInBN = JSBI.BigInt(amountIn.toString());
 
-console.log(trade);
-console.log(trade.transaction.data);
+  const amountPerPool = JSBI.divide(amountInBN, JSBI.BigInt(tokens.length));
 
-// once done with trade aka they have sent it and you don't need it anymore call
-trade.destroy();
+  function apiRequestUrl(methodName: any, queryParams: any) {
+    return apiBaseUrl + methodName + "?" + new URLSearchParams(queryParams).toString();
+  }
+
+  async function buildTxForSwap(swapParams: any) {
+    const url = apiRequestUrl("/swap", swapParams);
+
+    return fetch(url)
+      .then((res: any) => res.json())
+      .then((res: any) => res.tx);
+  }
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    const swapParams = {
+      fromTokenAddress: weth,
+      toTokenAddress: token.id,
+      amount: "100000000000000000",
+      fromAddress: arbiTokenEaterAddress,
+      slippage: 1,
+      disableEstimate: false,
+      allowPartialFill: false,
+    };
+
+    const swapTransaction = await buildTxForSwap(swapParams);
+
+    callData.push(swapTransaction);
+    tokenId.push(token.id);
+    value = JSBI.add(amountPerPool, value);
+  }
+
+  return [value, tokenId, callData];
+}
