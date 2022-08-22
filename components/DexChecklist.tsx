a7778959b7e2c126288f26b42ced9b76397a87bb
PSYTWEAK <psy@promise.exchange>
2022-08-22 15:45:32 +0100

    bug tweak

--- components/DexChecklist.tsx
+++ components/DexChecklist.tsx
@@ -5,8 +5,8 @@ export function DexCheckList({ config, setConfig }: { config: any; setConfig: an
   return (
     <div>
       <FormGroup>
-        <FormControlLabel control={<Checkbox defaultChecked={config.uniswap} onClick={() => setConfig({ uniswap: !config.uniswap })} />} label="Uniswap" />
-        <FormControlLabel control={<Checkbox defaultChecked={config.sushiswap} onClick={() => setConfig({ sushiswap: !config.sushiswap })} />} label="Sushiswap" />
+        <FormControlLabel control={<Checkbox defaultChecked={config.uniswap} onClick={() => setConfig({ ...config, uniswap: !config.uniswap })} />} label="Uniswap" />
+        <FormControlLabel control={<Checkbox defaultChecked={config.sushiswap} onClick={() => setConfig({ ...config, sushiswap: !config.sushiswap })} />} label="Sushiswap" />
       </FormGroup>
     </div>
   );
