1ce2fd44359ed576f04f8302aa01ab799ae25b09
PSYTWEAK <psy@promise.exchange>
2022-09-02 20:39:01 +0100

    minor refactoring

--- hooks/stratergies/filters.ts
+++ hooks/stratergies/filters.ts
@@ -15,8 +15,65 @@ export function shuffleTokens(array: any) {
 }
 
 export function removeDuplicates(tokens: any): any {
-  let _tokens = tokens.filter((value: any, index: number, self: any) => index === self.findIndex((t: any) => t.id === value.id));
+  return tokens.filter((value: any, index: number, self: any) => index === self.findIndex((t: any) => t.id === value.id));
+}
+
+
+export function removeStables(tokens: any): any {
+  let _tokens = tokens;
+  for (let i = 0; i < _tokens.length; i++) {
+    for (let j = 0; j < stables.length; j++) {
+      if (_tokens[i].name === stables[j]) {
+        _tokens.splice(i, 1);
+      }
+    }
+  }
   return _tokens;
+}
+
+
+export function removeBlueChips(tokens: any): any {
+  let _tokens = tokens;
+  for (let i = 0; i < _tokens.length; i++) {
+    for (let j = 0; j < blueChips.length; j++) {
+      if (_tokens[i].id === blueChips[j]) {
+        _tokens.splice(i, 1);
+      }
+    }
+  }
+  return _tokens;
+}
+
+
+export function removeLowVolume(tokens: any): any {
+  let _tokens = tokens;
+  for (let i = 0; i < _tokens.length; i++) {
+    if (_tokens[i].volumeUSD < lowVolume) {
+      _tokens.splice(i, 1);
+    }
+  }
+  return _tokens;
+}
+
+export function removeVolume(tokens: any): any {
+  let _tokens = tokens;
+  for (let i = 0; i < _tokens.length; i++) {
+    if (_tokens[i].volumeUSD > lowVolume) {
+      _tokens.splice(i, 1);
+    }
+  }
+  return _tokens;
+}
+
+export function removeNoneEthPools(pools: any): any {
+  let _pools = pools;
+  const index = _pools.findIndex((data: any) => data.pool.token0.id != weth && data.pool.token1.id != weth);
+  if (index > -1) {
+    _pools.splice(index, 1);
+    return removeNoneEthPools(_pools);
+  } else {
+    return _pools;
+  }
 }
 
 export function removeSignOfDerivInTokenName(tokens: any): any {
@@ -39,60 +96,4 @@ export function removeSignOfDerivInTokenName(tokens: any): any {
     }
   }
   return _tokens;
-}
-
-export function removeStables(tokens: any): any {
-  let _tokens = tokens;
-  for (let i = 0; i < stables.length; i++) {
-    const index = _tokens.findIndex((data: any) => data.id === stables[i] || data.id === stables[i]);
-    if (index > -1) {
-      _tokens.splice(index, 1);
-      i--;
-    }
-  }
-  return _tokens;
-}
-
-export function removeBlueChips(tokens: any): any {
-  let _tokens = tokens;
-  for (let i = 0; i < blueChips.length; i++) {
-    const index = _tokens.findIndex((data: any) => data.id === blueChips[i] || data.id === blueChips[i]);
-    if (index > -1) {
-      _tokens.splice(index, 1);
-      i--;
-    }
-  }
-  return _tokens;
-}
-
-export function removeLowVolume(tokens: any): any {
-  let _tokens = tokens;
-  const index = _tokens.findIndex((data: any) => data.volumeUSD < lowVolume);
-  if (index > -1) {
-    _tokens.splice(index, 1);
-    return removeLowVolume(_tokens);
-  } else {
-    return _tokens;
-  }
-}
-
-export function removeVolume(tokens: any): any {
-  let _tokens = tokens;
-  const index = _tokens.findIndex((data: any) => data.volumeUSD > lowVolume);
-  if (index > -1) {
-    _tokens.splice(index, 1);
-    return removeVolume(_tokens);
-  } else {
-    return _tokens;
-  }
-}
-export function removeNoneEthPools(pools: any): any {
-  let _pools = pools;
-  const index = _pools.findIndex((data: any) => data.pool.token0.id != weth && data.pool.token1.id != weth);
-  if (index > -1) {
-    _pools.splice(index, 1);
-    return removeNoneEthPools(_pools);
-  } else {
-    return _pools;
-  }
 }
