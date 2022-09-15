5b98d886066228f99143b127d6fcabbbc3eab948
PSYTWEAK <psy@promise.exchange>
2022-09-15 17:06:40 +0100

    temp

--- hooks/calldataForSwaps/externalTxBuilders/uniswap.ts
+++ hooks/calldataForSwaps/externalTxBuilders/uniswap.ts
@@ -7,9 +7,13 @@ export async function uniswap(provider: any, token: any, amountPerTrade: JSBI, s
 
   const router = new AlphaRouter({ chainId: provider._network.chainId, provider: provider });
 
-  const WETH = new Token(provider._network.chainId, weth, 18, "WETH", "Wrapped ETH");
+  //  const WETH = new Token(provider._network.chainId, weth, 18, "WETH", "Wrapped ETH");
 
+  //  const wethAmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(amountPerTrade.toString()));
 
+  const percentSlippage = new Percent(slippage, 100);
+
+  const TokenB = new Token(provider._network.chainId, token.id, 18, token.symbol, token.name);
 
 
   const route: any = false;
