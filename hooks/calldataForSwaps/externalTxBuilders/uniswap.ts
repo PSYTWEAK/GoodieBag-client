138595565ebb681b152a48a2163cadc7957db306
PSYTWEAK <psy@promise.exchange>
2022-09-15 17:26:22 +0100

    temp

--- hooks/calldataForSwaps/externalTxBuilders/uniswap.ts
+++ hooks/calldataForSwaps/externalTxBuilders/uniswap.ts
@@ -4,9 +4,9 @@ import JSBI from "jsbi";
 import { weth, arbiGoodieBagAddress, arbiUniswapRouterAddress } from "../../../globals";
 
 export async function uniswap(provider: any, token: any, amountPerTrade: JSBI, slippage: number, setTxObject: any) {
-  // @ts-ignore: Unreachable code error
 
-  const router2 = new AlphaRouter({ chainId: 42161, provider: provider });
+
+  const router = new AlphaRouter({ chainId: 42161, provider: provider });
 
 
   const WETH = new Token(provider._network.chainId, weth, 18, "WETH", "Wrapped ETH");
