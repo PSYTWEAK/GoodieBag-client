af0b79072ca87c96a763ed3b9af6763515c0c5a5
PSYTWEAK <psy@promise.exchange>
2022-09-15 12:52:29 +0100

    temp

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,20 @@ const Home: NextPage = () => {
           <Logo />
         </h1>
         <div className={styles.card}>
+          <CardHeader stratergy={stratergy} setStratergy={setStratergy} settingsActive={settingsActive} setSettingsActive={setSettingsActive} />
+
+          {stratergy && (
+            <>
+              {" "}
+              {!settingsActive && !settingsActive && (
+                <>
+                  <EtherInput amountETHIn={amountETHIn} setAmountETHIn={setAmountETHIn} />
+                  <BuyTokens tokens={tokens} setTokens={setTokens} loading={loading} slippage={slippage} amountETHIn={amountETHIn} generatingCalldata={generatingCalldata} setGeneratingCalldata={setGeneratingCalldata} />
+                  <Tokens tokens={tokens} loading={loading} setTokens={setTokens} />
+                </>
+              )}
+            </>
+          )}
 
         </div>
       </main>
