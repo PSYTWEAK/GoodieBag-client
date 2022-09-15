c63687e65434367c1b1155daa82bca2f29f5dc66
PSYTWEAK <psy@promise.exchange>
2022-09-15 17:18:42 +0100

    temp

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
+  optimization: {
+    minimize: false,
+    splitChunks: {
+      chunks: 'all',
+      name: true
+    },
+    runtimeChunk: true
+  },
 };
 
 module.exports = nextConfig;
