c3761e42e128fbe06b857161cdf4cafaf71fc64e
PSYTWEAK <psy@promise.exchange>
2022-09-15 16:27:35 +0100

    temp

--- hooks/calldataForSwaps/useGenerateCalldata.ts
+++ hooks/calldataForSwaps/useGenerateCalldata.ts
@@ -11,6 +11,41 @@ import { useEffect, useState } from "react";
 
 
 
+
+async function generateTokenSwapCalldata(token: any, index: number, setTokens: any, slippage: number, provider: any, amountPerTrade: JSBI, setTxObject: any) {
+
+  let success: boolean = false;
+
+  setTokens((prevState: any) => {
+    prevState[index].hasCalldata = "loading";
+    return [...prevState];
+  });
+
+  try {
+
+    if (slippage < 50) {
+      success = await oneInch(provider, token, amountPerTrade, slippage, setTxObject);
+    }
+
+    if (!success && token.protocol === "Uniswap V3") {
+
+      success = await uniswap(provider, token, amountPerTrade, slippage, setTxObject);
+    }
+    if (!success && token.protocol === "Sushiswap") {
+
+      success = await sushi(provider, token, amountPerTrade, slippage, setTxObject);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  setTokens((prevState: any) => {
+    prevState[index].hasCalldata = success.toString();
+    return [...prevState];
+  });
+
+}
+
 export default function useGenerateCalldata() {
 
   const [txObject, setTxObject] = useState({
