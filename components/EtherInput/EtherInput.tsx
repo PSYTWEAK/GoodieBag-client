10e25bc5ae9c51dec4e0405a128061f6a30dc016
PSYTWEAK <psy@promise.exchange>
2022-09-23 13:43:55 +0100

    added delay between updating the amountETHIn which can be used later to trigger the calldataBuilding so that callData isn't being built on every e.target.value update

--- components/EtherInput/EtherInput.tsx
+++ components/EtherInput/EtherInput.tsx
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/Home.module.css";
 import { TextField } from "@mui/material";
 import ETHIcon from "../ETHIcon";
+import useGenerateCalldata from "../../hooks/calldataForSwaps/useGenerateCalldata";
 export function EtherInput({ amountETHIn, setAmountETHIn }: { amountETHIn: any; setAmountETHIn: any }) {
+  const { txObject, generateCallData } = useGenerateCalldata();
+
+  const [storedAmountETHIn, setStoredAmountETHIn] = useState("");
+
+  useEffect(() => {
+    // wait 1 second and then do something
+    const timer = setTimeout(() => {
+      setStoredAmountETHIn(amountETHIn);
+    } // 1 second
+      , 1000);
+
+  }, [amountETHIn]);
+
+  useEffect(() => {
+
+    if (storedAmountETHIn === amountETHIn) {
+      console.log(storedAmountETHIn)
+    }
+
+
+  }, [storedAmountETHIn]);
+
+
   return (
     <>
       {" "}
