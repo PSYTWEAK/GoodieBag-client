193b1ccaf17305b2b58cc3cdce38f338d039aa2c
PSYTWEAK <psy@promise.exchange>
2022-10-12 21:44:41 +0100

    fixed price format

--- components/Tokens/TokenPrice.tsx
+++ components/Tokens/TokenPrice.tsx
@@ -9,24 +9,19 @@ import styles from "../../styles/Home.module.css";
 export function TokenPrice({ token, numberOfTokens, amountETHIn }: { token: any; numberOfTokens: number; amountETHIn: any }) {
     const provider = useProvider();
 
-    const [amountInThisToken, setAmountInThisToken] = useState(0);
     const [amountOutThisToken, setAmountOutThisToken] = useState("0");
     // use ethers to format units of token
 
     useEffect(() => {
         if (token.buyAmount) {
-            setAmountOutThisToken(token.buyAmount);
+            const buyAmount = Number(ethers.utils.formatUnits(token.buyAmount, 18));
+            setAmountOutThisToken(buyAmount.toLocaleString("en-US", { maximumFractionDigits: 2 }));
         } else if (token.hasCalldata === "true") {
 
             setAmountOutThisToken("?");
         }
 
     }, [token.buyAmount]);
-
-    useEffect(() => {
-        setAmountInThisToken(amountETHIn / numberOfTokens);
-
-    }, [numberOfTokens, amountETHIn]);
 
     return (<>
 
