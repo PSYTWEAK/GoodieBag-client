5eb67b37d06e6bf0957ce94e739e9143bce2a60a
PSYTWEAK <psy@promise.exchange>
2022-10-13 20:48:34 +0100

    tweaked socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,21 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import Head from "next/head";
-import TwitterIcon from '@mui/icons-material/Twitter';
-import { SocialIcon } from 'react-social-icons';
+
+import { FaTwitter, FaDiscord } from 'react-icons/fa'
 
 
 export function Footer() {
     return (
         <nav className={styles.footer}>
             <div className={styles.footerLinks}>
-                <SocialIcon url="https://twitter.com/TheDeadCuties" bgColor='transparent' fgColor='grey' style={{ height: 35, width: 35 }} />
-                <SocialIcon url="https://discord.gg/3ZTdrzHv" bgColor='rgb(240, 251, 255)' fgColor='grey' style={{ height: 35, width: 35 }} />
+                <a href='https://twitter.com/TheDeadCuties'>
+                    <FaTwitter style={{ height: 25, width: 25, color: "grey" }} />
+                </a>
+                <a href='https://discord.gg/3ZTdrzHv'>
+                    <FaDiscord style={{ height: 30, width: 30, color: "grey" }} />
+                </a>
+
             </div>
         </nav>
     );
