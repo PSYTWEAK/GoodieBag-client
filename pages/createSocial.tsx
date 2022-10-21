68571c7ecbb0ad8ee848a118029dff1538b7296e
PSYTWEAK <psy@promise.exchange>
2022-10-21 15:35:21 +0100

    create stratergy

--- pages/createSocial.tsx
+++ pages/createSocial.tsx
@@ -1,25 +1,37 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+
+let inputStyles = {
+    width: "4000px",
+    'input': {
+        color: 'white',
+        '&::placeholder': {
+            textOverflow: 'ellipsis !important',
+            color: 'white'
+        }
+    }
+}
 
 
 const CreateSocial: NextPage = () => {
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [tokens, setTokens] = useState(['', '', '']);
-    const [numberOfTokens, setNumberOfTokens] = useState(1);
-    const [tokenInputComponents, setTokenInputComponents] = useState(<></>);
 
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <div className={styles.card}>
-                    <h1 className={styles.title}>Create Social</h1>
+                    <h2 className={styles.cardTitleText}>Create Stratergy</h2>
                     <div className={styles.textField}>
                         <TextField
                             id="outlined-basic"
                             variant="standard"
+                            sx={inputStyles}
+                            className={styles.ETHinput}
                             InputProps={{
                                 disableUnderline: true,
                             }}
@@ -30,6 +42,8 @@ const CreateSocial: NextPage = () => {
                         <TextField
                             id="outlined-basic"
                             variant="standard"
+                            sx={inputStyles}
+                            className={styles.ETHinput}
                             InputProps={{
                                 disableUnderline: true,
                             }}
@@ -42,6 +56,8 @@ const CreateSocial: NextPage = () => {
                                 <TextField
                                     id="outlined-basic"
                                     variant="standard"
+                                    className={styles.ETHinput}
+                                    sx={inputStyles}
                                     onChange={(e) => {
                                         setTokens((prevTokens) => {
                                             prevTokens[index] = e.target.value;
@@ -52,11 +68,21 @@ const CreateSocial: NextPage = () => {
                                     InputProps={{
                                         disableUnderline: true,
                                     }}
-                                    placeholder="Token"
+                                    placeholder="Token Address"
                                 />
                             </div>
                         )
                     })}
+
+                    <Button className={styles.plusButton} >
+                        <AddIcon />
+                    </Button>
+
+                    <Button className={styles.buyButton} >
+                        Create Stratergy
+                    </Button>
+
+
                 </div>
             </main>
         </div>
